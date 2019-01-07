e9e83b7a925a858edae196a19c47f158246785b2
Masaya Nakashima <kawa18sima@gmail.com>
2019-01-07 17:46:30 +0900

    Update ImagesController.php
    Delete unnecessary actions
    Create image addition function

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -12,12 +12,8 @@ use App\Controller\AppController;
  */
 class ImagesController extends AppController
 {
+    public $components = ['Twitter'];
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
     public function index()
     {
         $this->paginate = [
@@ -28,85 +24,21 @@ class ImagesController extends AppController
         $this->set(compact('images'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Image id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $image = $this->Images->get($id, [
-            'contain' => ['Users']
-        ]);
-
-        $this->set('image', $image);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
-        $image = $this->Images->newEntity();
         if ($this->request->is('post')) {
-            $image = $this->Images->patchEntity($image, $this->request->getData());
-            if ($this->Images->save($image)) {
-                $this->Flash->success(__('The image has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
+            $user = $this->Auth->user();
+            $images = $this->Twitter->getTimeLineImages();
+            foreach($images as $url => $time){
+                $image = $this->Images->newEntity([
+                    'image_src' => $url,
+                    'created' => new DateTime($time),
+                    'user_id' => $user->id
+                ]);
+                $this->Images->save($image);
             }
-            $this->Flash->error(__('The image could not be saved. Please, try again.'));
         }
-        $users = $this->Images->Users->find('list', ['limit' => 200]);
-        $this->set(compact('image', 'users'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Image id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $image = $this->Images->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $image = $this->Images->patchEntity($image, $this->request->getData());
-            if ($this->Images->save($image)) {
-                $this->Flash->success(__('The image has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The image could not be saved. Please, try again.'));
-        }
-        $users = $this->Images->Users->find('list', ['limit' => 200]);
-        $this->set(compact('image', 'users'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Image id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $image = $this->Images->get($id);
-        if ($this->Images->delete($image)) {
-            $this->Flash->success(__('The image has been deleted.'));
-        } else {
-            $this->Flash->error(__('The image could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
+        return $this->redirect(['aciton' => 'index']);
+        
     }
 }
