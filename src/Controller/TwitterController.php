08b48c5c4be654b092e2ad2fec23abb3f098e26a
Masaya Nakashima <kawa18sima@gmail.com>
2019-01-09 16:44:43 +0900

    Update TwitterController.php
    Added processing to save to sns_acount table

--- src/Controller/TwitterController.php
+++ src/Controller/TwitterController.php
@@ -11,10 +11,19 @@ class TwitterController extends AppController
 
     public function beforeFilter(Event $event){
         $this->autoRender = false;
+        $this->loadModel('SnsAcount');
+        $current_user = $this->Auth->user();
     }
     
     public function callback(){
+        
         if($this->Twitter->initializeOnCallback()){
+            $acount = $this->SnsAcount->newEntity([
+                'acount_id' => $this->Twitter->getUserId(),
+                'sns' => 'twitter',
+                'user_id' => $current_user['id']
+            ]);
+            $this->SnsAcount->save($acount);
             $this->Flash->success(__('認証に成功しました。'));
         }else{
             $this->Flash->error(__('認証に失敗しました。'));
@@ -24,7 +33,7 @@ class TwitterController extends AppController
 
     public function login(){
         if ($this->request->is('get')){
-            if($this->Twitter->isAuthorized()){
+            if($this->SnsAcount->find()->where(['user_id' => $current_user['id'], 'sns' => 'twitter'])->count() > 0){
                 $this->Flash->error(__('すでにログインしています。'));
                 return $this->redirect(['controller' => 'images', 'action' => 'index']);
             }else{
