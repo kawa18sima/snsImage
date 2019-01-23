12a11d243598ce19622978decab0115a876765bc
Masaya Nakashima <kawa18sima@gmail.com>
2019-01-23 17:59:58 +0900

    Specification change of add and sync action
    add is twitter-only processing
    sync changes to other synchronization processing

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -23,12 +23,15 @@ class ImagesController extends AppController
         $this->set(compact('images'));
     }
 
-    public function add()
+    public function add($sync_id)
     {
         $this->autoRender = false;
         $this->loadModel('SnsAcounts');
-        $sns = $this->SnsAcounts->find()->where(['user_id' => $this->Auth->user()['id']])->first()['acount_id'];
         if ($this->request->is('post')) {
+            $sns = $this->SnsAcounts->find()->where(['sync' => $sync_id])->first();
+            if(isset($sns['id'])){
+                return $this->redirect(['action' => 'index']);
+            }
             $user = $this->Auth->user();
             $images = $this->Twitter->getTimeLineImages($sns);
             foreach($images as $url => $time){
@@ -36,16 +39,37 @@ class ImagesController extends AppController
                     'image_src' => $url,
                     'user_id' => $user['id']
                 ])->count() == 0){
-                $time = new Time($time);
-                $time->timezone = 'Asia/Tokyo';
-                $time->i18nFormat('yyyy-MM-dd HH:mm:ss');
-                $image = $this->Images->newEntity([
-                    'image_src' => $url,
-                    'upload_time' => $time,
-                    'user_id' => $user['id']
-                ]);
-                $this->Images->save($image);
+                    $time = new Time($time);
+                    $time->timezone = 'Asia/Tokyo';
+                    $time->i18nFormat('yyyy-MM-dd HH:mm:ss');
+                    $image = $this->Images->newEntity([
+                        'image_src' => $url,
+                        'upload_time' => $time,
+                        'user_id' => $user['id']
+                    ]);
+                    $this->Images->save($image);
+                }
             }
+
+            $sns['sync'] = null;
+            $this->SnsAcounts->save($sns);
+        }
+        return $this->redirect(['action' => 'index']);
+    }
+
+    public function sync(){
+        $this->autoRender = false;
+        $this->loadModel('SnsAcounts');
+        if($this->request->is('post')){
+            $current_user = $this->Auth->user();
+            $sns_acounts = $this->SnsAcounts->find()->where(['user_id' => $current_user['id']]);
+            foreach($sns_acounts as $sns_acount){
+                if($sns_acount['sns'] == 'twitter'){
+                    $sns_acount['sync'] = str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz');
+                    while(!($this->SnsAcounts->save($sns_acount))){
+                        $sns_acount['sync'] = str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz');
+                    }
+                }
             }
         }
         return $this->redirect(['action' => 'index']);
